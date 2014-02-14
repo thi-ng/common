e0a3ee8afdf334b56e0b76bd5f9eb9bc150009e1
Karsten Schmidt <k@postspectacular.com>
2014-02-14 15:01:09 +0000

    update tangle.sh, remove mkdir calls (now done via org-mode)

--- tangle.sh
+++ tangle.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 # -*- mode: shell-script -*-
-mkdir -p \
-babel/src-clj babel/src-cljs \
-babel/src-cljx/thi/ng/common/data \
-babel/src-cljx/thi/ng/common/math \
-babel/resources/public/js
 
 DIR=`pwd`
 FILES=""
@@ -14,11 +9,12 @@ for i in $@; do
     FILES="$FILES \"$i\""
 done
 
-/Applications/Emacs.app/Contents/MacOS/Emacs-10.7 -Q --batch \
-    --eval "(progn
-     (add-to-list 'load-path (expand-file-name \"~/.emacs.d/elpa/org-20130902/\"))
+emacs -Q --batch \
+    --eval \
+    "(progn
      (require 'org)(require 'ob)(require 'ob-tangle)
      (mapc (lambda (file)
             (find-file (expand-file-name file \"$DIR\"))
             (org-babel-tangle)
-            (kill-buffer)) '($FILES)))" #2>&1 | grep Tangled
+            (kill-buffer)) '($FILES)))" \
+#2>&1 | grep Tangled
